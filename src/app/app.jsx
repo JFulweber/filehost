615d9fff3f5135019f3fb6b05cabdecdf0ef2cee
Fulweber <FULWEJAM000@mysbisd.org>
2018-01-12 13:52:00 -0600

    whatever dude

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,14 +1,11 @@
 import reACT from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from 'pages/home/Home.jsx'
+//import Home from 'pages/home/Home.jsx'
 
-class App extends reACT.Component {
+export default class App extends reACT.Component {
     render(){
         return(
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                //<Route component={404}/>
-            </Switch>
+            <p> hi jumbalia fondler </p>
         )
     }
 }
