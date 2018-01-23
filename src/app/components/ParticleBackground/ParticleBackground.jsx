fa191428fef92094ecd3a1442ecf98004244cd1a
Fulweber <FULWEJAM000@mysbisd.org>
2018-01-23 13:49:58 -0600

    pushin

--- src/app/components/ParticleBackground/ParticleBackground.jsx
+++ src/app/components/ParticleBackground/ParticleBackground.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import style from './background.scss';
-import Part from '../Particles/Particles.jsx';
+import Part from 'react-particles-js';
 import params from '../Particles/ParticleSettings';
 
 export default class ParticleBackground extends React.Component{
     render(){
         return( 
-            <div>
-                <Part/>
-                <div className={style.foreground}>
+            <div id="container" className={style.container}>
+                <div className={style.divParticle}>
+                    <Part params={params} id='particles' className={style.particles}/>
+                </div>
+                <div className={style.overlay}>
                     {this.props.children}
                 </div>
             </div>
--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
         return(
             <div id="home-main" className={style.Home}>
                 <ParticleBackground>
-                    yes hello
+                    what is UP bro
                 </ParticleBackground>
             </div>
         )
