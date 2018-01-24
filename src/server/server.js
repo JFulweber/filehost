26a3caf2bccece2b069fd8f3167271e63647357d
Fulweber <FULWEJAM000@mysbisd.org>
2018-01-24 15:03:16 -0600

    commit testing

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,25 @@
+var childProcess = require('child_process');
+
+/*
+    SETTING UP MONGO CHILD PROCESS
+*/
+
+var mongoprocess = childProcess.fork('./src/server/modules/mongohandle.js');
+console.log(mongoprocess)
+mongoprocess.on('stdio',data=>{console.log(`From mongo child process ${data}`)})
+
 var express = require('express');
+
 var PORT = 3000;
 
 var app = express();
+
 
 app.use(express.static('./dist/'))
 
 
 
 app.listen(PORT,function(){
-    console.log('hello reACT');
+    console.log('hello react');
 })
 
