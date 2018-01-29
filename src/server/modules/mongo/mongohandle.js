0e15e10de1ca563a5b04a0c9ab167015d3f1f535
Fulweber <FULWEJAM000@mysbisd.org>
2018-01-29 14:56:32 -0600

    aaaaaa

--- src/server/modules/mongo/mongohandle.js
+++ src/server/modules/mongo/mongohandle.js
@@ -1,2 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://localhost:27017').then((status,err)=>{
+    console.log(status);
+    if(err) console.log(err); 
+}).catch((err)=>{
+    console.log(`Dissa'n error`);
+    console.log(err);
+});
