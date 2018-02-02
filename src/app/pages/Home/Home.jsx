cb704a62c420403cd6136534d681859dd9ee70b2
Horn <HORNFRE000@mysbisd.org>
2018-02-02 15:00:36 -0600

    clean

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import ParticleBackground from '../../components/ParticleBackground/ParticleBack
 import Login from '../../components/Login/Login.jsx';
 import ColorBlock from '../../components/ColorBlock/ColorBlock.jsx';
 import palette from '../../palette.scss'; 
+import Title from '../../components/Title/Title.jsx';
 
 export default class Home extends React.Component {
     render() {
         return(
             <div id="home-main" className={style.Home}>
                 <ParticleBackground>
+                    <Title/>
                     <Login/>
                     <div className={style.blocks}>
                         <ColorBlock className={style.left} color={palette.colorRed} width='100vw' height='30vh' skew='-65deg'/>
