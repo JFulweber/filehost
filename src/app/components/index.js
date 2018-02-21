2758219f1c320c7d89d66a9db696d5f42e5256d9
Fulweber <FULWEJAM000@mysbisd.org>
2018-02-21 13:59:13 -0600

    eskertet

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,2 +1,11 @@
-import autoExports from 'auto-exports';
-module.exports = autoExports(__dirname);
+import About from './About/About.jsx';
+import Login from './Login/Login.jsx';
+import Logout from './Logout/Logout.jsx';
+import NavElement from './NavElement/NavElement.jsx';
+import NotFound from './NotFound/NotFound.jsx';
+import Particle from './Particle/Particle.jsx';
+import Register from './Register/Register.jsx';
+import Title from './Title/Title.jsx';
+import Uploader from './Uploader/Uploader.jsx';
+
+module.exports = {About, Login, Logout, NavElement, NotFound, Particle, Register, Title, Uploader};
