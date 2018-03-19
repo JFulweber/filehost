67908494423db7ba7bcc6f79d14c5f09f51b2f41
Horn <HORNFRE000@mysbisd.org>
2018-03-19 14:08:40 -0500

    whatever

--- src/app/components/FileList/FileList.jsx
+++ src/app/components/FileList/FileList.jsx
@@ -8,20 +8,30 @@ export default class FileList extends React.Component {
         var render = {}
         return (
             <div className={styles.fileContainer}>
+                <div className={styles.header}>
+                    <h1 className={styles.name}>Name</h1>
+                    <h1 className={styles.size}>Size</h1>
+                    <h1 className={styles.type}>Type</h1>
+                </div>
                 <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
-                <FileElement fileName='TOP' fileSize='TOP' type='TOP' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
+                <FileElement fileName='MIDDLE' fileSize='MIDDLE' type='MIDDLE' />
                 <FileElement fileName='BOTTOM' fileSize='BOTTOM' type='BOTTOM' />
-            </div>  
+            </div>
         )
     }
 }
