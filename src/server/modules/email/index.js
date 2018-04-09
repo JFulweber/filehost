7b6cab179bf71fd7f041486e5a51375dcb5e1b26
Fulweber <FULWEJAM000@mysbisd.org>
2018-04-09 14:48:13 -0500

    push

--- src/server/modules/email/index.js
+++ src/server/modules/email/index.js
@@ -1,6 +1,10 @@
 var nodemailer = require('nodemailer');
 var creds = require('./credentials');
 var smtpTransport = require('nodemailer-smtp-transport');
+var realEmails ='adairf625@gmail.com, myleshorn@gmail.com';
+var devEmails = 'fulwejam000@mysbisd.org';
+var urlPrefix = 'localhost:3000/registerUser/';
+var uuid = require('uuid');
 
 var transporter = nodemailer.createTransport(smtpTransport({
   service: 'gmail',
@@ -12,9 +16,20 @@ var transporter = nodemailer.createTransport(smtpTransport({
 }));
 
 var mailOptions = {
-    from: creds.email,
-    to: 'fulwejam000@mysbisd.org, hornfre000@mysbisd.org',
-    subject: 'what up lol',
-    text: ' what up lol '
+  from: creds.email,
+  to: 'fulwejam000@mysbisd.org, hornfre000@mysbisd.org',
+  subject: 'what up lol',
+  text: ' what up lol '
 }
 
+module.exports = {
+  register: function (user) {
+    var mailOptions = {
+      from: creds.email,
+      to: devEmails,
+      subject: `Registration Request`,
+      text: `There has been a registration request from user ${user.username}, to accept it click this link: ${urlPrefix+user.registrationHash}`
+    }
+    transporter.sendMail(mailOptions);
+  }
+}
