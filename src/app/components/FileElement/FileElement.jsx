268bd663a8c969c907471e14e9a707d2f3fab4e0
Myles Horn <myles@MilkyWay.com>
2018-04-13 14:53:24 -0500

    xcvmkl

--- src/app/components/FileElement/FileElement.jsx
+++ src/app/components/FileElement/FileElement.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from './FileElement.scss';
 import FileTools from '../../components/FileTools/FileTools.jsx';
+import styles from './FileElement.scss';
 
 
 export default class FileElement extends React.Component {
